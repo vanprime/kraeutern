8cdf17b9e37d24eb7184a7b4199b3f7381def909
Dustin van Prime <25372772+vanprime@users.noreply.github.com>
2023-12-27 00:29:16 +0100

    added motion to pubquiz

--- src/components/Quiz/Quiz.jsx
+++ src/components/Quiz/Quiz.jsx
@@ -3,14 +3,28 @@ import useQuiz from '@/hooks/useQuiz';
 import { useEffect } from 'react';
 import { Button } from '@/components/ui/button';
 import { ArrowLeft, ArrowRight, Play } from 'lucide-react';
+import { motion } from 'framer-motion';
+
+const fadeInUp = {
+    initial: { opacity: 0, y: 20 },
+    animate: { opacity: 1, y: 0 },
+    exit: { opacity: 0, y: -20 }
+};
+
 
 const InterruptScreen = ({ title, description, callback }) => {
     return (
         <div className='flex flex-col p-9 my-auto bg-muted rounded text-center mx-auto'>
-            <div>
-                <h2 className='font-semibold tracking-wider text-[5rem] text-slate-50'>{title}</h2>
+            <h2 className='font-semibold tracking-wider text-[5rem] text-slate-50'>{title}</h2>
+            <motion.div
+                variants={fadeInUp}
+                initial="initial"
+                animate="animate"
+                exit="exit"
+                transition={{ duration: 0.5 }}
+            >
                 {description && <p className='text-muted-foreground'>{description}</p>}
-            </div>
+            </motion.div>
             <div className='flex justify-center mt-9'>
                 <Button onClick={callback}><Play /></Button>
             </div>
@@ -77,11 +91,27 @@ const Quiz = ({ game }) => {
                 ) : (
                     <div className='flex flex-1 flex-col'>
                         <div className='p-9 rounded bg-grad flex flex-1 flex-col justify-center border-slate-500 '>
-                            <h2 className='font-semibold tracking-wide text-[3rem] text-indigo-950 text-center'>{questions[currentQuestionIndex].question}</h2>
+                            <motion.h2
+                                key={questions[currentQuestionIndex].question}
+                                className='font-semibold tracking-wide text-[3rem] text-indigo-950 text-center'
+                                variants={fadeInUp}
+                                initial="initial"
+                                animate="animate"
+                                exit="exit"
+                                transition={{ duration: 0.5 }}
+                            >{questions[currentQuestionIndex].question}
+                            </motion.h2>
                             {showSolution && (
-                                <div className="p-9">
+                                <motion.div
+                                    variants={fadeInUp}
+                                    initial="initial"
+                                    animate="animate"
+                                    exit="exit"
+                                    transition={{ duration: 0.5 }}
+                                    className="p-9"
+                                >
                                     <p className='font-semibold tracking-wide text-[3rem] text-fuchsia-800 text-center'>{questions[currentQuestionIndex].solution}</p>
-                                </div>
+                                </motion.div>
                             )}
                         </div>
                         <div className='pt-4 flex justify-between mt-auto'>
