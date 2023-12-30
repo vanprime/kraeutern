24e10848d581389f056ae2a7a75275468969b2ac
Dustin van Prime <25372772+vanprime@users.noreply.github.com>
2023-12-30 14:59:21 +0100

    added vite config

--- vite.config.js
+++ vite.config.js
@@ -1,15 +1,27 @@
+import { join, parse, resolve } from "path"
 import path from "path"
 import { defineConfig, loadEnv } from 'vite'
 import react from '@vitejs/plugin-react'
 
 
+
+function entryPoints(...paths) {
+  const entries = paths.map(parse).map(entry => {
+    const { dir, base, name } = entry;
+    const key = join(dir, name);
+    const path = resolve(__dirname, dir, base);
+    return [key, path];
+  });
+
+  const config = Object.fromEntries(entries);
+  return config;
+}
+
 // https://vitejs.dev/config/
 export default defineConfig(({ command, mode }) => {
-  // Load env file based on `mode` in the current working directory.
-  // Set the third parameter to '' to load all env regardless of the `VITE_` prefix.
-  const env = loadEnv(mode, process.cwd(), '')
+  const env = loadEnv(mode, process.cwd(), '');
+
   return {
-    // vite config
     plugins: [react()],
     base: '/kraeutern/',
     resolve: {
@@ -20,5 +32,13 @@ export default defineConfig(({ command, mode }) => {
     define: {
       __APP_ENV__: JSON.stringify(env.APP_ENV),
     },
+    build: {
+      rollupOptions: {
+        input: entryPoints(
+          "index.html", // Your main entry point
+          "404.html"    // Your custom 404 page
+        ),
+      },
+    },
   }
-})
+});
