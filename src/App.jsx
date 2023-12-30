94fc88f898590d86d42681f13c95f367d5b1b410
Dustin van Prime <25372772+vanprime@users.noreply.github.com>
2023-12-30 15:55:06 +0100

    added referer text to main

--- src/App.jsx
+++ src/App.jsx
@@ -3,9 +3,12 @@ import { ThemeProvider } from '@/providers/theme-provider';
 
 function App() {
 
+  const referrer = document.referrer;
+
   return (
     <ThemeProvider defaultTheme="dark" storageKey="vite-ui-theme">
       <div className='flex min-h-dvh flex-col'>
+        {referrer && <h1>{referrer}</h1>}
         <Outlet />
       </div>
     </ThemeProvider>
