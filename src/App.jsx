976dd622f33e0a42146a4b04a4e0cc91d9ad1a9f
Dustin van Prime <25372772+vanprime@users.noreply.github.com>
2023-12-30 16:14:12 +0100

    moved theme provider to main

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,11 @@
 import { Outlet } from 'react-router-dom';
-import { ThemeProvider } from '@/providers/theme-provider';
 
 function App() {
 
-  const referrer = document.referrer;
-
   return (
-    <ThemeProvider defaultTheme="dark" storageKey="vite-ui-theme">
-      <div className='flex min-h-dvh flex-col'>
-        {referrer && <h1>{referrer}</h1>}
-        <Outlet />
-      </div>
-    </ThemeProvider>
+    <div className='flex min-h-dvh flex-col'>
+      <Outlet />
+    </div>
   )
 }
 
--- src/main.jsx
+++ src/main.jsx
@@ -16,6 +16,7 @@ import Teamselect from "@/pages/Teamselect.jsx";
 import Buzzer from "@/pages/Buzzer.jsx";
 import Buzzerlayout from "@/layouts/Buzzerlayout.jsx";
 import { GamestateProvider } from "./providers/gamestate-provider.jsx";
+import { ThemeProvider } from '@/providers/theme-provider';
 
 const baseUrl = '/kraeutern/'
 
@@ -63,11 +64,13 @@ const router = createBrowserRouter([
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <GamesProvider>
-      <GamestateProvider>
-        <RouterProvider router={router} />
-      </GamestateProvider>
-    </GamesProvider>
-    <Toaster />
+    <ThemeProvider defaultTheme="dark" storageKey="vite-ui-theme">
+      <GamesProvider>
+        <GamestateProvider>
+          <RouterProvider router={router} />
+        </GamestateProvider>
+      </GamesProvider>
+      <Toaster />
+    </ThemeProvider>
   </React.StrictMode>
 );
