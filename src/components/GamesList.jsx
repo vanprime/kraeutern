b23222af5cf678537f248a85f6606961c893d78a
Dustin van Prime <25372772+vanprime@users.noreply.github.com>
2024-01-05 15:01:20 +0100

    added images to gamelist

--- src/components/GamesList.jsx
+++ src/components/GamesList.jsx
@@ -1,21 +1,54 @@
 import { useGames } from '@/providers/games-provider';
-import { Button } from '@/components/ui/button';
+import { useState, useEffect } from 'react';
 import { Link } from 'react-router-dom';
-import { Play } from 'lucide-react';
+import { Loader2 } from 'lucide-react';
+
+
+const GameImage = ({ slug, name }) => {
+    const [isLoaded, setIsLoaded] = useState(false);
+    const [hasError, setHasError] = useState(false);
+    const cmsPath = process.env.VITE_CMS_PATH;
+
+    useEffect(() => {
+        const img = new Image();
+        img.src = `${cmsPath}/gamelogos/${slug}.webp`;
+        img.onload = () => setIsLoaded(true);
+        img.onerror = () => setHasError(true);
+    }, [slug]);
+
+    if (hasError) {
+        return (
+            <img
+                src="${cmsPath}/gamelogos/default.webp"
+                alt={name ? name : 'Kraeutern.'}
+                className="aspect-square object-cover rounded-xl"
+            />)
+    }
+
+    return isLoaded ? (
+        <img
+            src={`${cmsPath}/gamelogos/${slug}.webp`}
+            alt={name}
+            className="aspect-square object-cover rounded-xl"
+        />
+    ) : (
+        <div className="aspect-square object-cover rounded-xl flex items-center justify-center">
+            <Loader2 className='animate-spin' />
+        </div>
+    );
+}
 
 const GamesList = () => {
     const { games } = useGames()
     return (
         <>
             {games.map(game => (
-                <div key={game.slug} className="flex flex-col flex-grow flex-shrink-0 basis-40 min-h-56 p-4 rounded-md bg-muted">
-                    <p className='text-xl'>{game.name}</p>
+                <div key={game.slug} className="flex flex-col flex-grow flex-shrink-0 basis-40 min-h-56 max-w-96 justify-between">
+                    <Link to={`/games/${game.slug}`} className='hover:outline rounded-xl my-2'>
+                        <GameImage slug={game.slug} name={game.name} />
+                    </Link>
+                    <p className='text-xl mb-2'>{game.name}</p>
                     <p className='text-muted-foreground text-sm'>{game.description}</p>
-                    <div className="mt-auto" >
-                        <Button asChild className="w-full">
-                            <Link to={`/games/${game.slug}`}><Play /></Link>
-                        </Button>
-                    </div>
                 </div>
             ))}
         </>
